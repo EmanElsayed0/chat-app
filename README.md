9073c8070ce43ae1aa7a74d6b1b5e7cf4eee436a
Noran Tarek <ntsm2001@gmail.com>
2022-05-19 01:42:04 +0200

    time

---
+++ README.md
@@ -0,0 +1,5 @@
+# chat-example
+
+# for-IS-3-FCI-SCU-Students
+
+Check Deployed Demo Heroku:
---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require ('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 2000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port,() =>{
+    console.log (`Socket.IO server running at http://localhost:${port}/`);
+});
